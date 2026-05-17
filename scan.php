bbccf88d474dd9cfc12f7bdfcb90987c9c4dd6af
robbytrujillo <robbyikusuma88@gmail.com>
2026-05-18 05:25:32 +0700

    Introduction Farmogana Prototype

--- scan.php
+++ scan.php
@@ -7,6 +7,8 @@
 
     <title>farmogana Scanner</title>
 
+    <link rel="icon" type="image/x-icon" href="/farmogana/assets/images/logo.png">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
